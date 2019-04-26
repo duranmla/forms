a3561607509e34ad4f2e4c6b48c1f5cfc577747c
Alexis Duran <alexis.duranm@gmail.com>
2019-04-26 08:43:13 +0200

    chore(schema): progress into General Dispute Debt schema

--- src/MozillaForm.js
+++ src/MozillaForm.js
@@ -1,21 +1,13 @@
 import React from "react";
 import Form from "react-jsonschema-form";
-
-const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: { type: "string", title: "Title", default: "A new task" },
-    done: { type: "boolean", title: "Done?", default: false }
-  }
-};
+import generalDispute from "./generalDispute";
 
 const log = type => console.log.bind(console, type);
 
 export default () => (
   <Form
-    schema={schema}
+    // uiSchema={uiSchema}
+    schema={generalDispute}
     onChange={log("changed")}
     onSubmit={log("submitted")}
     onError={log("errors")}
---
+++ src/generalDispute.js
@@ -0,0 +1,113 @@
+export default {
+  definitions: {
+    DebtTypes: {
+      type: "string",
+      anyOf: [
+        {
+          title: "Federal student loan debt",
+          const: "federalStudentLoanDebt"
+        },
+        {
+          title: "Credit card debt",
+          const: "creditCardDebt"
+        }
+      ]
+    },
+    USAStates: {
+      type: "string",
+      anyOf: [
+        {
+          title: "Alabama",
+          const: "foo"
+        },
+        {
+          title: "Florida",
+          const: "bar"
+        }
+      ]
+    }
+  },
+  $schema: "http://json-schema.org/schema#",
+  title: "Dispute Any Debt in Collections",
+  description:
+    "This dispute is for all types of debt in collections except student loans.",
+  type: "object",
+  required: [
+    "address",
+    "agency-address",
+    "agency-city",
+    "agency-name",
+    "agency-state",
+    "agency-zip-code",
+    "city",
+    "collection-notice-date",
+    "debts",
+    "name",
+    "state",
+    "zip-code"
+  ],
+  properties: {
+    address: {
+      type: "string",
+      title: "Your Mailing Address"
+    },
+    "agency-address": {
+      type: "string",
+      title: "Collection agency’s or law firm’s mailing address"
+    },
+    "agency-city": {
+      type: "string",
+      title: "Collection agency’s or law firm’s City"
+    },
+    "agency-name": {
+      type: "string",
+      title: "Name of collection agency or law firm"
+    },
+    "agency-state": {
+      $ref: "#/definitions/USAStates",
+      title: "Collection agency’s or law firm’s State"
+    },
+    "agency-zip-code": {
+      type: "string",
+      title: "Collection agency’s or law firm’s Zip Code",
+      pattern: "[0-9]{5}"
+    },
+    city: {
+      type: "string",
+      title: "Your City"
+    },
+    "collection-notice-date": {
+      type: "string",
+      title: "Collection notice date",
+      format: "date"
+    },
+    debts: {
+      title: "Amount of Debt Disputed",
+      description:
+        "Please provide the amount of debt collectors claim you owe. This will help us better understand the types of debt you and our members are fighting. ",
+      type: "object",
+      properties: {
+        debtType: {
+          title: "Debt type",
+          $ref: "#/definitions/DebtTypes"
+        },
+        debtAmount: {
+          type: "number",
+          title: "Amount"
+        }
+      }
+    },
+    name: {
+      type: "string",
+      title: "Your Full Name"
+    },
+    state: {
+      title: "Your State",
+      $ref: "#/definitions/USAStates"
+    },
+    "zip-code": {
+      type: "string",
+      pattern: "[0-9]{5}"
+    }
+  }
+};
